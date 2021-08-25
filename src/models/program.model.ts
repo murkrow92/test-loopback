53918b95f59a77b2b71ab2c70c154fb6c9675cac
murkrow <ducdt1992@gmail.com>
2021-08-25 14:59:18 +0700

    add program models

---
+++ src/models/program.model.ts
@@ -0,0 +1,26 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Program extends Entity {
+  @property({
+    type: 'string',
+    required: true,
+  })
+  title: string;
+
+  @property({
+    type: 'string',
+  })
+  description?: string;
+
+
+  constructor(data?: Partial<Program>) {
+    super(data);
+  }
+}
+
+export interface ProgramRelations {
+  // describe navigational properties here
+}
+
+export type ProgramWithRelations = Program & ProgramRelations;
